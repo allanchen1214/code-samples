421a10add80974368fc3e00eda3f799eaeb83cab
allanchen <allanchen@tencent.com>
2022-12-26 15:29:29 +0800

    basic - atomic

---
+++ golang/basic/atomic/go.mod
@@ -0,0 +1,3 @@
+module code-samples/golang/basic/atomic
+
+go 1.19
---
+++ golang/basic/atomic/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"sync/atomic"
+)
+
+//func AddT(addr *T, delta T)(new T)
+//func LoadT(addr *T) (val T)
+//func StoreT(addr *T, val T)
+//func SwapT(addr *T, new T) (old T)
+//func CompareAndSwapT(addr *T, old, new T) (swapped bool)
+
+type Page struct {
+	views uint32
+}
+
+func (page *Page) SetViews(n uint32) {
+	atomic.StoreUint32(&page.views, n)
+}
+
+func (page *Page) Views() uint32 {
+	return atomic.LoadUint32(&page.views)
+}
+
+type MyData struct {
+	iValue   uint32
+	strValue string
+	arrs     []uint32
+	m        map[string]string
+}
+
+var myDataValue atomic.Value
+
+func main() {
+	var page Page
+	page.SetViews(2022)
+	fmt.Println(page.Views())
+
+	data := new(MyData)
+	data.iValue = 2022
+	data.strValue = "sss"
+	data.arrs = make([]uint32, 0)
+	data.arrs = append(data.arrs, 1)
+	data.arrs = append(data.arrs, 2)
+	data.arrs = append(data.arrs, 3)
+	data.m = make(map[string]string)
+	data.m["firstname"] = "chen"
+	data.m["lastname"] = "wufang"
+	myDataValue.Store(data)
+
+	storeVal := myDataValue.Load()
+	if storeVal != nil {
+		storeData := storeVal.(*MyData)
+		fmt.Println(storeData)
+	}
+}
